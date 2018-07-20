dddc409d92c10862b32914a46c6205fc82786dfe
SamGonzalez28 <samantha.gonzalez@unl.edu.ec>
2018-07-20 08:10:28 -0500

    Script Base de Datos 2

--- Nose/scripBase.sql
+++ Nose/scripBase.sql
@@ -20,28 +20,25 @@ CREATE TABLE `nose`.`cliente` (
   `direccion` VARCHAR(200) NULL,
   `telefono` VARCHAR(15) NULL,
   `external_id` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+  PRIMARY KEY (`id`));
   
-  
-CREATE TABLE `nose`.`menu` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `tipo` VARCHAR(100) NOT NULL,
-  `precio` DECIMAL(6,2) NOT NULL,
-  `descripcion` VARCHAR(200) NULL,
-  `external_id` VARCHAR(100) NOT NULL,
-  `id_local` INT(11) NOT NULL,
+    
+    CREATE TABLE `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` varchar(100) NOT NULL,
+  `precio` decimal(6,2) NOT NULL,
+  `descripcion` varchar(200) DEFAULT NULL,
+  `external_id` varchar(100) NOT NULL,
+  `id_local` int(11) NOT NULL,
+  `fecha_crea` timestamp NULL DEFAULT NULL,
+  `fecha_act` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `id_local_idx` (`id_local` ASC) VISIBLE,
-  CONSTRAINT `id_local`
-    FOREIGN KEY (`id_local`)
-    REFERENCES `nose`.`local` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  FOREIGN KEY (`id_local`) REFERENCES `local` (`id`)
+) ;
     
     CREATE TABLE `nose`.`registros` (
   `id` INT NOT NULL,
-  `id_user` INT(11) NOT NULL,
+  `id_cliente` INT(11) NOT NULL,
   `id_menu` INT(11) NULL,
   `cantidad` SMALLINT(3) NULL,
   `fecha` TIMESTAMP,
@@ -50,34 +47,26 @@ CREATE TABLE `nose`.`menu` (
   `saldo_final` DECIMAL(6,2) NULL,
   `external_id` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `id_user_idx` (`id_user` ASC) VISIBLE,
-  INDEX `id_menu_idx` (`id_menu` ASC) VISIBLE,
-  CONSTRAINT `id_user`
-    FOREIGN KEY (`id_user`)
-    REFERENCES `nose`.`cliente` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `id_menu`
+
+    FOREIGN KEY (`id_cliente`)
+    REFERENCES `nose`.`cliente` (`id`),
+   
+ 
     FOREIGN KEY (`id_menu`)
     REFERENCES `nose`.`menu` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    );
     
-    CREATE TABLE `nose`.`login` (
+    CREATE TABLE `nose`.`sesion` (
   `id_cliente` INT(11) NULL,
   `id_local` INT(11) NULL,
-  INDEX `id_local_idx` (`id_local` ASC) VISIBLE,
-  INDEX `id_cliente_idx` (`id_cliente` ASC) VISIBLE,
-  CONSTRAINT `id_local1`
+  
     FOREIGN KEY (`id_local`)
     REFERENCES `nose`.`local` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `id_cliente1`
+    ,
+  
     FOREIGN KEY (`id_cliente`)
     REFERENCES `nose`.`cliente` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    );
     
     CREATE TABLE `cartera` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -85,10 +74,11 @@ CREATE TABLE `nose`.`menu` (
   `id_cliente` int(11) NOT NULL,
   `saldo` decimal(6,2) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `id_local_idx` (`id_local`),
-  KEY `id_cliente_idx` (`id_cliente`),
-  CONSTRAINT `id_cliente1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
-  CONSTRAINT `id_local1` FOREIGN KEY (`id_local`) REFERENCES `local` (`id`)
+ 
+  FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
+  FOREIGN KEY (`id_local`) REFERENCES `local` (`id`)
 ) ;
+
+
 
 
